b4228eaadb31678d43d6c4aa9c138d3ea2f6e691
Marcelo Guerra <mwguerra@gmail.com>
2021-04-05 17:48:21 -0300

    Implementada a criação e listagem de transações

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce((sum, current) => {
+      const currentIncome = (current.type === 'income') ? current.value : 0
+      const currentOutcome = (current.type === 'outcome') ? current.value : 0
+
+      return {
+        income: sum.income + currentIncome,
+        outcome: sum.outcome + currentOutcome,
+        total: sum.total + currentIncome - currentOutcome
+      }
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    })
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, value, type })
+
+    if (type === 'outcome' && value > this.getBalance().total) {
+      throw Error('Outcome exceeds current total balance.')
+    }
+
+    this.transactions.push(transaction)
+
+    return transaction
   }
 }
 
